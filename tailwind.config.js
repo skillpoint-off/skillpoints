c8cd8bc552a6666e34fa817b86d633d572ab748b
Kuberan1810 <144237380+Kuberan1810@users.noreply.github.com>
2026-02-02 23:23:54 +0530

    Resolve tailwind,config  conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
